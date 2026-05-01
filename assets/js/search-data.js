d1bb63b55d4c37aa57c8c88024ef4ad8aaf75498
kangmj0128 <kangwg0128@gmail.com>
2026-05-01 06:53:05 +0000

    Deploying to gh-pages from @ kangmj0128/kangmj0128.github.io@5214480ea32c2e224e4cbc79486650da9a629cdd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "",
+          description: "My full CV. You can also download the PDF.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
